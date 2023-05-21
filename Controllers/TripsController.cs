e64eb6455f02f2761082d3b9e810cc7b441097cb
MikhAndrey <mikhalevange@gmail.com>
2023-05-21 22:15:10 +0300

    Добавьте файлы проекта.

---
+++ Controllers/TripsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Andrei_Mikhaleu_Task1.Controllers
+{
+    public class TripsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/RouteBuilder.js
@@ -0,0 +1,111 @@
+﻿let map;
+let markers = [];
+let directionsService;
+let directionsDisplay;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 52.42509,
+            lng: 30.8685096
+        },
+        zoom: 12
+    });
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+    map.addListener('click', function (event) {
+        addMarker(event.latLng);
+    });
+}
+
+function addMarker(location) {
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+    
+    const label = String.fromCharCode(65 + markers.length);
+    const letterMarker = new google.maps.Marker({
+        position: location,
+        icon: getLetterMarkerIcon(label),
+        map: map
+    });
+    markers.push({
+        marker: marker,
+        letterMarker: letterMarker,
+        label: label
+    });
+    letterMarker.addListener('click', function () {
+        const index = getMarkerIndexByLabel(label);
+        if (index > -1) {
+            markers[index].marker.setMap(null);
+            markers[index].letterMarker.setMap(null);
+            markers.splice(index, 1);
+            calculateAndDisplayRoute();
+        }
+    });
+    letterMarker.setDraggable(true);
+    google.maps.event.addListener(letterMarker, 'dragend', function (event) {
+        const index = getMarkerIndexByLabel(label);
+        if (index > -1) {
+            markers[index].marker.setPosition(event.latLng);
+            calculateAndDisplayRoute();
+        }
+    });
+    calculateAndDisplayRoute();
+}
+
+function getLetterMarkerIcon(label) {
+    return {
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: '#ff0000',
+        fillOpacity: 1,
+        strokeColor: '#000000',
+        strokeWeight: 1,
+        scale: 7,
+        labelOrigin: new google.maps.Point(0, 3),
+        label: label
+    };
+}
+
+function getMarkerIndexByLabel(label) {
+    for (let i = 0; i < markers.length; i++) {
+        if (markers[i].label === label) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function calculateAndDisplayRoute() {
+    if (markers.length < 2) {
+        directionsDisplay.setDirections({
+            routes: []
+        });
+        return;
+    }
+    const start = markers[0].marker;
+    const end = markers[markers.length - 1].marker;
+    const waypoints = [];
+    for (let i = 1; i < markers.length - 1; i++) {
+        waypoints.push({
+            location: markers[i].marker.position,
+            stopover: true
+        });
+    }
+    const request = {
+        origin: start.position,
+        destination: end.position,
+        waypoints: waypoints,
+        travelMode: 'DRIVING'
+    };
+    directionsService.route(request, function (response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
+google.maps.event.addDomListener(window, 'load', initMap);
